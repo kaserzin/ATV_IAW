a23b5c0f46a2632cb2d7859c3d1c4fee84cfabd4
kaser <110909102+kaserzin@users.noreply.github.com>
2024-10-22 21:59:09 -0300

    Delete

--- scripts/fundamentos-questao02.js
+++
@@ -1,7 +0,0 @@
-function calcAreaTri(a, b, c) {
-    const s = (a + b + c) / 2;
-    return Math.sqrt(s * (s - a) * (s - b) * (s - c));
-}
-
-const area = calcAreaTri(2, 3, 4);
-console.log(`A área do triângulo é: ${area.toFixed(2)}cm²`);
